84c516380aee4983db952f83a33de998c108076a
Rosalba <romemo15@gmail.com>
2025-04-10 08:01:19 -0500

    feat se crean repositorios

---
+++ src/main/java/com/example/classRoomAPI/modelos/Curso.java
@@ -0,0 +1,48 @@
+package com.example.classRoomAPI.modelos;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_curso")
+    private Integer id;
+
+    @Column(name="nombre", length = 100, unique = false, nullable = false)
+    private String nombre;
+
+    //Creando relacion (muchos a 1)
+    @ManyToOne
+    @JoinColumn(name="fk_docente", referencedColumnName = "id")
+    @JsonBackReference
+    private Docente docente;
+
+    public Curso() {
+    }
+
+    public Curso(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
---
+++ src/main/java/com/example/classRoomAPI/modelos/Docente.java
@@ -0,0 +1,45 @@
+package com.example.classRoomAPI.modelos;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+public class Docente {
+
+    @Column(name="id_docente")
+    private Integer id;
+    private  String especialidad;
+
+    //creando relacion (1 a muchos)
+    @OneToMany(mappedBy = "docente")
+    @JsonManagedReference
+    private List<Curso> cursos;
+
+
+    public Docente() {
+    }
+
+    public Docente(Integer id, String especialidad) {
+        this.id = id;
+        this.especialidad = especialidad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+}
